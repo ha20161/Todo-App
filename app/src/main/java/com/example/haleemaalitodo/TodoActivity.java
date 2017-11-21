54ccfb478ee9d72d442fd9a83c9b7a51f37600f8
Haleema Ali <Haleema@Haleemas-MacBook-Pro.local>
2017-11-21 13:35:31 +0000

    rotation problem fix

--- app/src/main/java/com/example/haleemaalitodo/TodoActivity.java
+++ app/src/main/java/com/example/haleemaalitodo/TodoActivity.java
@@ -3,6 +3,7 @@ package com.example.haleemaalitodo;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +15,22 @@ public class TodoActivity extends AppCompatActivity {
     private String[] mTodos;
     private int mTodoIndex = 0;
 
+    public static final String TAG = "TodoActivity";
+
+    // In case of state change, due to rotating the phone
+    // store the mTodoIndex to display the same mTodos element post state change
+    // N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
+    // the alternative is to abstract view data to a ViewModel which can be in scope in all
+    // Activity states and more suitable for larger amounts of data
+    private static final String TODO_INDEX = "com.example.todoIndex";
+    // override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,9 +38,17 @@ public class TodoActivity extends AppCompatActivity {
         // the view hierarchy
         super.onCreate(savedInstanceState);
 
+        Log.d(TAG, " **** Just to say the PC is in onCreate!");
+
         // set the user interface layout for this Activity
         // the layout file is defined in the project res/layout/activity_todo.xml file
         setContentView(R.layout.activity_todo);
+
+        // check for saved state due to changes such as rotation or back button
+        // and restore any saved state such as the todo index
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
 
         // initialize member TextView so we can manipulate it later
         final TextView TodoTextView;
